a358e69da3121ffcabaee7b437d61e7538b9fb1e
Ervin R Radosavlevici <ervin210@icloud.com>
2025-02-23 19:08:33 +0000

    Update main.go
    Signed-off-by: Ervin R Radosavlevici <ervin210@icloud.com>

--- cmd/asset-syncer/main.go
+++ cmd/asset-syncer/main.go
@@ -1,10 +1,1 @@
-// Copyright 2021-2022 the Kubeapps contributors.
-// SPDX-License-Identifier: Apache-2.0
 
-package main
-
-import "github.com/vmware-tanzu/kubeapps/cmd/asset-syncer/cmd"
-
-func main() {
-	cmd.Execute()
-}
